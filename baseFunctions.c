faa4a01f4a59f64ebb281f97e48325b07dc251af
Charles Holtforster <charles.holtforster@gmail.com>
2016-11-16 14:38:25 -0500

    Split constants into seperate header file

--- baseFunctions.c
+++ baseFunctions.c
@@ -1,17 +1,5 @@
-#pragma debuggerWindows("joystickSimple");
-#include "JoystickDriver.c"
 
-#define XAXIS motorA
-#define YAXIS motorB
-
-#define XENDSTOP S1
-#define YENDSTOP S2
-
-const float mmPerDeg = 0.32; // Distance per encoder count.
-const float xMax = 230;
-const float yMax = 300;
-
-const int maxPower = 25;
+#include "constants.h"
 
 void resetAxis(int axis) {
 	nMotorEncoder[axis] = 0;
---
+++ constants.h
@@ -0,0 +1,16 @@
+// Main declaration header for NXTPlotter
+
+#pragma debuggerWindows("joystickSimple");
+#include "JoystickDriver.c"
+
+#define XAXIS motorA
+#define YAXIS motorB
+
+#define XENDSTOP S1
+#define YENDSTOP S2
+
+const float mmPerDeg = 0.32; // Distance per encoder count.
+const float xMax = 230;
+const float yMax = 300;
+
+const int maxPower = 25;
